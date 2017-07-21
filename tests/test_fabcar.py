0203181aa5f1a00bbc29ea70c17f0638e64473b1
Chris Poole <chrispoole@uk.ibm.com>
2017-07-21 16:55:55 +0100

    Use unittest module

--- tests/test_fabcar.py
+++ tests/test_fabcar.py
@@ -20,86 +20,44 @@
 
 from sys import argv
 from fabric_rest import FabricRest
-import subprocess
-import traceback
 import base64
+import unittest
 
-testsPassed = 0
-testsFailed = 0
 
-def test_first_channel_name():
-    """Test to confirm that the first channel on the network has the default name of
-    mychannel.
-    """
-    global testsFailed
-    try:
+class TestFabCar(unittest.TestCase):
+
+    def test_first_channel_name(self):
+        """Test to confirm that the first channel on the network has the default name of
+        mychannel.
+        """
         first_channel = restserver.get_channels()["channels"][0]["channel_id"]
-        _equals(first_channel, "mychannel", "First channel name test")
-    except subprocess.CalledProcessError as err:
-        testsFailed += 1
-        print "\n! test_first_channel_name : failed \n!    Stack Trace:\n"
-        traceback.print_exc()
+        self.assertEqual(first_channel, "mychannel")
 
 
-def test_car_color():
-    global testsFailed
-    try:
+    def test_car_color(self):
         """Test to confirm that the first car returned from a ledger query is the color blue."""
         car_color = restserver.query_ledger("mychannel", chaincode_id="fabcar",
                                             data=r'{"fcn":"queryAllCars","args":[]}')["queryResult"][0]["Record"]["colour"]
-        _equals(car_color, "blue", "Car color test")
-    except subprocess.CalledProcessError as err:
-        testsFailed += 1
-        print "\n! test_car_color : failed  \n!    Stack Trace:\n"
-        traceback.print_exc()
+        self.assertEqual(car_color, "blue")
 
-def test_peer_chaincode_query():
-    global testsFailed
-    try:
+
+    def test_peer_chaincode_query(self):
         """Test to confirm that querying a peer for fabcar chaincode returns a result and querying for fab does not."""
         chaincode_name = restserver.query_chaincode("fabcar","%5B0%5D")["queryResult"][0]["name"]
-        _equals(chaincode_name, "fabcar", "Peer chaincode query test")
-    except subprocess.CalledProcessError as err:
-        testsFailed += 1
-        print "\n! test_peer_chaincode_query : failed \n!    Stack Trace:\n"
-        traceback.print_exc()
-    try:
-        empty_result = restserver.query_chaincode("fab","%5B0%5D")["queryResult"][0]
-        _equals(empty_result, {}, "Peer chaincode not found query test")
-    except subprocess.CalledProcessError as err:
-        testsFailed += 1
-        print "\n! test_peer_chaincode_query - not found: failed \n!    Stack Trace:\n"
-        traceback.print_exc()
+        self.assertEqual(chaincode_name, "fabcar")
 
-def test_chaincode_install():
-    """Test to confirm that installing chaincode on a peer works."""
-    global testsFailed
-    global testsPassed
-    try:
+        empty_result = restserver.query_chaincode("fab","%5B0%5D")["queryResult"][0]
+        self.assertEqual(empty_result, {})
+
+
+    def test_chaincode_install(self):
+        """Test to confirm that installing chaincode on a peer works."""
         # install chaincode: id, path(in archive), archive as base64 string, version, peers
         archiveFile = open('input/installTest.tar.gz', 'rb')
         archiveInb64 = base64.b64encode(archiveFile.read())
         install_result = restserver.install_chaincode("marbles","marbles02/marbles_chaincode.go",archiveInb64,"1.0","%5B0%5D")["peerResponses"]
         #install_result = restserver.install_chaincode_file("marbles","marbles02/marbles_chaincode.go","1.0","%5B0%5D")["peerResponses"]
         # print install_result
-        testsPassed += 1
-    except (subprocess.CalledProcessError,IOError,TypeError,KeyError) as err:
-        testsFailed += 1
-        print "\n! test_chaincode_install: failed \n!    Stack Trace:\n"
-        traceback.print_exc()
-
-
-
-def _equals(a, b, test_name):
-    """Confirm that values a and b are equal, printing text to the screen."""
-    global testsPassed
-    global testsFailed
-    if a == b:
-        testsPassed+=1
-        print test_name + ": passed"
-    else:
-        testsFailed+=1
-        print "! " + test_name + ": failed"
 
 
 if __name__ == "__main__":
@@ -112,11 +70,4 @@ if __name__ == "__main__":
 
     restserver = FabricRest(hostname, port)
 
-    test_first_channel_name()
-    test_car_color()
-    test_peer_chaincode_query()
-
-    # COMMENTED OUT as needs to sort output parsing and running a second time.
-    #test_chaincode_install()
-
-    print "\nSummary:\n   Passed:  " + str(testsPassed) + "\n   Failed:  " + str(testsFailed)
+    unittest.main()
